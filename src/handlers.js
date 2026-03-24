9ffecb0706fc5e6f25389aa0370821c7c9256610
Anna <annaspacer7@gmail.com>
2026-03-24 14:02:52 +0300

    feat: player registration and login

--- src/handlers.js
+++ src/handlers.js
@@ -1,7 +1,42 @@
+const { players, games, sockets, genId, genCode } = require('./store');
+const { send, broadcast } = require('./utils');
+
+const socketToPlayer = new Map();
+const playerPasswords = new Map();
+
 function handleMessage(ws, socketId, msg) {
+  const { type, data } = msg;
+  switch (type) {
+    case 'reg': return handleReg(ws, socketId, data);
+    default: break;
+  }
+}
+
+function handleReg(ws, socketId, data) {
+  const { name, password } = data;
+
+  let existing = null;
+  for (const [idx, p] of players) {
+    if (p.name === name) { existing = [idx, p]; break; }
+  }
+
+  if (existing) {
+    const [idx, p] = existing;
+    if (playerPasswords.get(idx) !== password) {
+      return send(ws, 'reg', { name, index: '', error: true, errorText: 'Wrong password' });
+    }
+    socketToPlayer.set(socketId, idx);
+    return send(ws, 'reg', { name, index: idx, error: false, errorText: '' });
+  }
+
+  const index = genId();
+  players.set(index, { name, index, score: 0 });
+  playerPasswords.set(index, password);
+  socketToPlayer.set(socketId, index);
+  send(ws, 'reg', { name, index, error: false, errorText: '' });
 }
 
 function handleDisconnect(socketId) {
 }
 
-module.exports = { handleMessage, handleDisconnect };
+module.exports = { handleMessage, handleDisconnect, socketToPlayer };
